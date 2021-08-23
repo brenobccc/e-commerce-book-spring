e54c59e24884ae178d71b3a51195f71918d8ec30
brenobccc <brenobritobc@gmail.com>
2021-08-23 14:54:58 -0300

    inicioprojeto

---
+++ src/test/java/br/edu/ifce/lp2/ecommercebook/ECommerceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.lp2.ecommercebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
